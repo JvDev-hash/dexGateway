331174594046108d9265033cdcb5151ea1545b9e
João Victor <jvbaprof@gmail.com>
2023-03-13 13:47:54 -0300

    initial commit

---
+++ src/main/java/poke/dex/dexGateway/DexGatewayApplication.java
@@ -0,0 +1,13 @@
+package poke.dex.dexGateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DexGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DexGatewayApplication.class, args);
+	}
+
+}
